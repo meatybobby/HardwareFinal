f1f694a359a899ed97419f9a383528136637e2ff
meatybobby <meatybobby@gmail.com>
2015-06-25 16:50:12 +0800

    Visible

--- calculator.v
+++ calculator.v
@@ -1,11 +1,20 @@
-module calculator(clk,rst);
+module calculator(LCD_ENABLE, LCD_RW, LCD_DI, LCD_CS1,LCD_CS2, LCD_RST, LCD_DATA,clk,rst);
 input clk,rst;
-reg[1:0] line;
-reg[11:0] exp1,exp2,exp3,exp4,exp5,exp6,tmp_exp;
-reg[15:0] delay;
-reg[3:0] ans1,ans2,ans3;
-generator g(tmp_exp,line,clk,rst);
-always @(posedge clk or negedge rst) begin
+output LCD_ENABLE,LCD_RW,LCD_DI,LCD_CS1,LCD_CS2,LCD_RST;
+output [7:0]  LCD_DATA;
+reg[11:0] exp1,exp2,exp3,exp4,exp5,exp6;
+reg[9:0] delay;
+reg[9:0] ans1,ans2,ans3;
+reg[15:0] score;
+reg[1:0] life;
+wire delay_clk,LCD_ENABLE,LCD_RW,LCD_DI,LCD_CS1,LCD_CS2, LCD_RST;
+wire[1:0] line,in_line;
+wire[7:0] in_ans,LCD_DATA;
+wire[11:0] tmp_exp;
+generator g(tmp_exp,line,delay_clk,rst);
+clk_16 c(delay_clk,clk,rst);
+display d(clk,rst,LCD_ENABLE, LCD_RW, LCD_DI, LCD_CS1,LCD_CS2, LCD_RST, LCD_DATA,exp1,exp2,exp3,exp4,exp5,exp6);
+always @(posedge delay_clk or negedge rst) begin
 	if(!rst) begin
 		exp1 = 12'h000;
 		exp2 = 12'h000;
@@ -13,11 +22,51 @@ always @(posedge clk or negedge rst) begin
 		exp4 = 12'h000;
 		exp5 = 12'h000;
 		exp6 = 12'h000;
-		delay <= 16'h0000;
+		score <= 16'h0000;
+		life <= 2'b10;
+		delay <= {2'b00,8'h00};
 	end
-	else if(!delay[15]) delay <= delay+1;
+	else if(in_line!=2'b00) begin
+		if(in_line==2'b01) begin
+			if(in_ans==ans1) begin
+				if(exp4!=12'h000) begin
+					score <= score+5;
+					exp4 = 12'h000;
+				end
+				else if(exp1!=12'h000) begin
+					score <= score+5;
+					exp1 = 12'h000;
+				end
+			end
+		end
+		else if(in_line==2'b10) begin
+			if(in_ans==ans2) begin
+				if(exp5!=12'h000) begin
+					score <= score+5;
+					exp5 = 12'h000;
+				end
+				else if(exp2!=12'h000) begin
+					score <= score+5;
+					exp2 = 12'h000;
+				end
+			end
+		end
+		else begin
+			if(in_ans==ans3) begin
+				if(exp6!=12'h000) begin
+					score <= score+5;
+					exp6 = 12'h000;
+				end
+				else if(exp3!=12'h000) begin
+					score <= score+5;
+					exp3 = 12'h000;
+				end
+			end
+		end
+	end
+	else if(!delay[9]) delay <= delay+1;
 	else begin
-		delay <= <= 16'h0000;
+		delay <= {2'b00,8'h00};
 		exp4 = exp1;
 		exp5 = exp2;
 		exp6 = exp3;
@@ -77,4 +126,15 @@ always @(posedge clk or negedge rst) begin
 		end
 	end
 end
+endmodule
+
+module clk_16(delay_clk,clk,rst);
+input clk,rst;
+output delay_clk;
+reg[15:0] divider;
+always@(posedge clk or negedge rst) begin
+	if(!rst) divider<=16'h0000;
+	else divider<=divider+1;
+end
+assign delay_clk=divider[15];
 endmodule
